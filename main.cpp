b4915c44f8c6ce20d7a5cc0a7e022bd7edf97a83
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-06-15 12:39:45 +0200

    Minor cleanup

--- main.cpp
+++ main.cpp
@@ -16,28 +16,10 @@ using namespace ssvu::Encryption;
 using namespace ssvu::FileSystem;
 using namespace ssvuj;
 
-using Request = Http::Request;
-using Response = Http::Response;
-using Status = Http::Response::Status;
+struct Target { string remoteFolder, localFolder; };
+struct DownloadData { bool localExists; string remotePath, remoteMD5, localPath, localMD5; };
 
-struct Target
-{
-	string remoteFolder, localFolder;
-	Target(const string& mRemoteFolder, const string& mLocalFolder) : remoteFolder{getNormalizedPath(mRemoteFolder)}, localFolder{getNormalizedPath(mLocalFolder)} { }
-};
-
-struct DownloadData
-{
-	bool localExists;
-	string remotePath, remoteMD5;
-	string localPath, localMD5;
-
-	DownloadData(bool mLocalExists, const string& mRemotePath, const string& mRemoteMD5, const string& mLocalPath, const string& mLocalMD5) :
-		localExists{mLocalExists}, remotePath{mRemotePath}, remoteMD5{mRemoteMD5}, localPath{mLocalPath}, localMD5{mLocalMD5} { }
-};
-
-string backupFolder{"_BACKUP/"};
-string host, hostMainFolder, hostMainConfig, hostMainScript, remoteDataFolder;
+string backupFolder{"_BACKUP/"}, host, hostMainFolder, hostMainConfig, hostMainScript, remoteDataFolder;
 vector<string> remoteExcludedFiles, remoteExcludedFolders, remoteOnlyNewFiles;
 vector<Target> targets;
 vector<DownloadData> downloadDatas;
@@ -64,17 +46,9 @@ vector<string> getFolderNames(const string& mPath)
 string downloadFileContents(const string& mRemotePath)
 {
 	log("Getting <" + mRemotePath + "> from server...", "downloadFileContents");
-
-	Response response{Http(host).sendRequest(mRemotePath)};
-
-	if(response.getStatus() == Response::Ok)
-	{
-		log("<" + mRemotePath + "> got successfully", "downloadFileContents");
-		return response.getBody();
-	}
-
-	log("Get <" + mRemotePath + "> error", "downloadFileContents");
-	return {};
+	Http::Response response{Http(host).sendRequest(mRemotePath)};
+	if(response.getStatus() == Http::Response::Ok) { log("<" + mRemotePath + "> got successfully", "downloadFileContents"); return response.getBody(); }
+	log("Get <" + mRemotePath + "> error", "downloadFileContents"); return "";
 }
 
 void download(const DownloadData& mDownloadData)
@@ -83,125 +57,75 @@ void download(const DownloadData& mDownloadData)
 
 	if(mDownloadData.localExists)
 	{
-		log("Backing up <" + mDownloadData.localPath + ">", "download");
+		log("Backing up <" + mDownloadData.localPath + "> to <" + backupFolder + mDownloadData.localPath + ">", "download");
+		if(!exists(backupFolder)) createFolder(backupFolder);
 		for(auto& f : getFolderNames(mDownloadData.localPath)) if(!exists(backupFolder + f)) createFolder(backupFolder + f);
 		ofstream ofs{backupFolder + mDownloadData.localPath, ofstream::binary};
-		ofs << getFileContents(mDownloadData.localPath);
-		ofs.flush(); ofs.close();
+		ofs << getFileContents(mDownloadData.localPath); ofs.flush(); ofs.close();
 	}
 
-	for(auto& f : getFolderNames(mDownloadData.localPath))
-		if(!exists(f))
-		{
-			log("Creating folder <" + f + ">", "download");
-			createFolder(f);
-		}
+	for(auto& f : getFolderNames(mDownloadData.localPath)) if(!exists(f)) { log("Creating folder <" + f + ">", "download"); createFolder(f); }
 
 	ofstream ofs{mDownloadData.localPath, ofstream::binary};
-	ofs << downloadFileContents(hostMainFolder + mDownloadData.remotePath);
-	ofs.flush(); ofs.close();
-
+	ofs << downloadFileContents(hostMainFolder + mDownloadData.remotePath); ofs.flush(); ofs.close();
 	log("Finished processing <" + mDownloadData.localPath + ">", "download");
 }
 
 void loadLocalConfig()
 {
-	log("");
-	log("loading local config...", "loadLocalConfig");
-	log("");
+	log(""); log("loading local config...", "loadLocalConfig"); log("");
 
 	const Json::Value localConfig{getRootFromFile("updaterConfig.json")};
-
-	host			= as<string>(localConfig, "host");
-	log("host: <" + host + ">", "loadLocalConfig");
-
-	hostMainFolder	= as<string>(localConfig, "hostMainFolder");
-	log("hostMainFolder: <" + hostMainFolder + ">", "loadLocalConfig");
-
-	hostMainConfig	= as<string>(localConfig, "hostMainConfig");
-	log("hostMainConfig: <" + hostMainConfig + ">", "loadLocalConfig");
-
-	hostMainScript	= as<string>(localConfig, "hostMainScript");
-	log("hostMainScript: <" + hostMainScript + ">", "loadLocalConfig");
+	host			= as<string>(localConfig, "host"); log("host: <" + host + ">", "loadLocalConfig");
+	hostMainFolder	= as<string>(localConfig, "hostMainFolder"); log("hostMainFolder: <" + hostMainFolder + ">", "loadLocalConfig");
+	hostMainConfig	= as<string>(localConfig, "hostMainConfig"); log("hostMainConfig: <" + hostMainConfig + ">", "loadLocalConfig");
+	hostMainScript	= as<string>(localConfig, "hostMainScript"); log("hostMainScript: <" + hostMainScript + ">", "loadLocalConfig");
 
 	for(const auto& t : localConfig["targets"])
 	{
-		auto remoteFolder(as<string>(t, "remoteFolder"));
-		auto localFolder(as<string>(t, "localFolder"));
-
-		targets.emplace_back(remoteFolder, localFolder);
+		auto remoteFolder(as<string>(t, "remoteFolder")), localFolder(as<string>(t, "localFolder"));
+		targets.push_back({getNormalizedPath(remoteFolder), getNormalizedPath(localFolder)});
 		log("target: <" + remoteFolder + "> -> <" + localFolder + ">", "loadLocalConfig");
 	}
 }
 
 void loadRemoteConfig()
 {
-	log("");
-	log("loading remote config...", "loadRemoteConfig");
-	log("");
+	log(""); log("loading remote config...", "loadRemoteConfig"); log("");
 
 	const Json::Value remoteConfig{getRootFromString(downloadFileContents(hostMainFolder + hostMainConfig))};
-
-	remoteDataFolder	= as<string>(remoteConfig, "dataFolder");
-	log("remoteDataFolder: <" + remoteDataFolder + ">", "loadRemoteConfig");
-
-	for(const auto& f : as<vector<string>>(remoteConfig, "excludedFiles"))
-	{
-		remoteExcludedFiles.push_back(f);
-		log("remoteExcludedFile: <" + f + ">", "loadRemoteConfig");
-	}
-
-	for(const auto& f : as<vector<string>>(remoteConfig, "excludedFolders"))
-	{
-		remoteExcludedFolders.push_back(f);
-		log("remoteExcludedFolder: <" + f + ">", "loadRemoteConfig");
-	}
-
-	for(const auto& f : as<vector<string>>(remoteConfig, "onlyNewFiles"))
-	{
-		remoteOnlyNewFiles.push_back(f);
-		log("remoteOnlyNewFile: <" + f + ">", "loadRemoteConfig");
-	}
+	remoteDataFolder = as<string>(remoteConfig, "dataFolder"); log("remoteDataFolder: <" + remoteDataFolder + ">", "loadRemoteConfig");
+	for(const auto& f : as<vector<string>>(remoteConfig, "excludedFiles"))		{ remoteExcludedFiles.push_back(f);		log("remoteExcludedFile: <" + f + ">", "loadRemoteConfig"); }
+	for(const auto& f : as<vector<string>>(remoteConfig, "excludedFolders"))	{ remoteExcludedFolders.push_back(f);	log("remoteExcludedFolder: <" + f + ">", "loadRemoteConfig"); }
+	for(const auto& f : as<vector<string>>(remoteConfig, "onlyNewFiles"))		{ remoteOnlyNewFiles.push_back(f);		log("remoteOnlyNewFile: <" + f + ">", "loadRemoteConfig"); }
 }
 
 void loadRemoteScript()
 {
-	log("");
-	log("loading remote script...", "loadRemoteScript");
-	log("");
+	log(""); log("loading remote script...", "loadRemoteScript"); log("");
 
 	const Json::Value remoteScriptResult{getRootFromString(downloadFileContents(hostMainFolder + hostMainScript))};
 
 	for(auto& f : remoteScriptResult)
 	{
-		auto remotePath(as<string>(f, "path"));
-		auto remoteMD5(as<string>(f, "md5"));
-
+		auto remotePath(as<string>(f, "path")), remoteMD5(as<string>(f, "md5"));
 		log("remoteFiles: <" + remotePath + "> <" + remoteMD5 + ">", "loadRemoteScript");
 
-		auto localPath(getReplaced(remotePath, remoteDataFolder, ""));
-		bool localExists{false};
-		string localMD5{""};
+		string localPath{getReplaced(remotePath, remoteDataFolder, "")}, localMD5{""};
 		for(const auto& t : targets) replace(localPath, t.remoteFolder, t.localFolder);
+		bool localExists{exists(localPath)};
 
 		if(isFolder(localPath)) continue;
-		if(exists(localPath))
-		{
-			localExists = true;
-			localMD5 = encrypt<Encryption::Type::MD5>(getFileContents(localPath));
-			log("localFiles: <" + localPath + "> <" + localMD5 + ">", "loadRemoteScript");
-		}
+		if(localExists) { localMD5 = encrypt<Encryption::Type::MD5>(getFileContents(localPath)); log("localFiles: <" + localPath + "> <" + localMD5 + ">", "loadRemoteScript"); }
 		else log("localFiles: <" + localPath + "> <does not exist>", "loadRemoteScript");
 
-		downloadDatas.emplace_back(localExists, remotePath, remoteMD5, localPath, localMD5);
+		downloadDatas.push_back({localExists, remotePath, remoteMD5, localPath, localMD5});
 	}
 }
 
 void processDownloads()
 {
-	log("");
-	log("processing downloads...", "processDownloads");
-	log("");
+	log(""); log("processing downloads...", "processDownloads"); log("");
 
 	for(const auto& d : downloadDatas)
 	{
@@ -211,14 +135,4 @@ void processDownloads()
 	}
 }
 
-int main()
-{
-	loadLocalConfig();
-	loadRemoteConfig();
-	loadRemoteScript();
-	processDownloads();
-
-	saveLogToFile("updaterLog.txt");
-
-	return 0;
-}
+int main() { loadLocalConfig(); loadRemoteConfig(); loadRemoteScript(); processDownloads(); saveLogToFile("updaterLog.txt"); return 0; }
